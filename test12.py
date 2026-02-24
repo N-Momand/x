6a52b5ca6b174c12eb740d4617a60554b9eb2616
Naseem Momand <naseem.momand@student.howest.be>
2026-02-24 12:42:04 +0100

    if else

--- test12.py
+++ test12.py
@@ -1,1 +1,7 @@
- Dit is een test n12
+naam = input ("Wat is jou naam ? ")
+if naam == "Naseem" :
+    print ("Welkom boss")  
+elif naam == "naseem" :
+    print ("Je bent welkom")
+else :
+    print (" Je bent niet welkom ")
